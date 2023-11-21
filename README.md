09da7beaa7023df70660626f58982bcd72e5a4d4
serena-peschiaroli <serena.peschiaroli@gmail.com>
2023-11-21 15:17:19 +0100

    Fix: generate email, changing method with a for loop

--- README.md
+++ README.md
@@ -7,7 +7,11 @@ Far comparire gli indirizzi email solamente quando sono stati tutti generati.
 
 **Soluzione**
 
-1. Inserire nuovo array : [] in return dove inserire i dati generati; creare method per generare 1 email random usando l'API indicata + Axios ;
-2. per generare 10 mail, aggiungere in return items : 10, modificare l'api con ?items=${this.items};
-3. Implementare in HTML un v-if sull'elemento <ul> per fare in modo che la lista sia visibile solo e soltanto se l'array "email" non sia vuoto
-4. Implementare all'interno di HTML un v-for per iterare i dati nel nuovo array generato tramite axios;
+1. Inserire nuovo array : [] in return dove inserire i dati generati; 
+2. inserire un counter = 0 in return per azzerare il loop;
+3. inserire in return items = 10 per generare 10 email;
+4. creare metodo generaEmail con un loop for all'interno per gestire la richiesta 10 volte:
+5. se il counter < di item=10 --> richiedere il dato ad axios e pusharlo nell'array vuoto; aggiornare il counter;
+6. se il counter === items, terminare loop e loggare risultato
+7. Implementare in HTML un v-if sull'elemento <ul> per fare in modo che la lista sia visibile solo e soltanto se l'array "email" non sia vuoto
+8. Implementare all'interno di HTML un v-for per iterare i dati nel nuovo array generato tramite axios;
--- js/script.js
+++ js/script.js
@@ -5,19 +5,40 @@ createApp({
     data(){
         
         return{
-            email: [],
+            // Array per memorizzare gli indirizzi email generati
+            emails: [],
+              // Numero di email da generare
             items: 10,
+            // Contatore per tracciare le richieste completate
+            completedReq: 0,
 
         };
     },
     methods:{
+        // Metodo per generare gli indirizzi email
         generaEmail(){
-            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-            .then(response => {
-                this.email= response.data;
-                console.log(response.data);
-
-            })
+            // Azzera l'array di email quando si avvia la generazione
+            this.emails= []
+            // Loop per effettuare le richieste di generazione email
+            for (let i = 0; i < this.items; i++){
+                // Verifica se il counter è minore delle email da generare
+                if (this.completedReq < this.items){
+                    // Effettua una richiesta al servizio API per ottenere un indirizzo email casuale
+                    axios.get(`https://flynn.boolean.careers/exercises/api/random/mail`)
+                    .then(response => {
+                        // Aggiunge l'indirizzo email all'array
+                    this.emails.push(response.data.response);
+                    // Incrementa il contatore 
+                    this.completedReq++;
+                        // Se il contatore è === ad items, stampa l'array di email
+                    if(this.completedReq === this.items) {
+                        console.log(this.emails);
+                    }
+                    });
+                }
+                
+            }
+            
         }
 
     },
